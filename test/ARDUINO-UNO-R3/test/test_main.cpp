d5c5aabeaf052be66f8f89179eb65bc03ca46b52
vinelopre <mail@vinelopre.com>
2024-06-03 10:45:45 -0400

    Reorganized folders for testing environment.

---
+++ test/ARDUINO-UNO-R3/test/test_main.cpp
@@ -0,0 +1,22 @@
+#include <unity.h>
+#include <Arduino.h>
+
+void setUp()
+{
+    // set up stuff here (before)
+}
+
+void tearDown()
+{
+    // clean stuff up here (after)
+}
+
+void setup()
+{
+
+}
+
+void loop()
+{
+
+}
