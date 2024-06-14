be11bc3cf8f40809e7c5afcced99f8817c727b37
vinelopre <mail@vinelopre.com>
2024-06-13 23:43:23 -0400

    Added a test for nRF chips. Also modified potentiometer test to check for dynamic manual input. Added an analog read test for pins as well.

---
+++ test/ARDUINO_UNO_R3/lib/test_radio/test_radio.cpp
@@ -0,0 +1,60 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <unity.h>
+#include <binary.h>
+
+RF24 radio(7, 8); // CE, CSN
+const byte ADDRESS = B000001;
+const int TEST_DATA[10] = {0,1,2,3,4,5,6,7,8,9};
+
+void test_chip_call (void){
+    bool test = radio.begin();
+    Serial.println(test);
+    TEST_ASSERT_EQUAL(test, true);   
+}
+
+void test_byte_address(void){
+    radio.openWritingPipe(ADDRESS);
+
+    //switched from protected to public in RF24.h line 148 in order to use read_register.
+    byte test = radio.read_register(RX_ADDR_P0);
+    Serial.println(test);
+    TEST_ASSERT_EQUAL_MEMORY(ADDRESS, test, sizeof(ADDRESS));
+
+    test = radio.read_register(TX_ADDR);
+    Serial.println(test);
+    TEST_ASSERT_EQUAL_MEMORY(ADDRESS, test, sizeof(ADDRESS));
+}
+
+void test_write(void){
+    radio.openWritingPipe(ADDRESS);
+    const int *ptr;
+    
+    bool test;
+    for(int i = 0; i <10; i++){
+        ptr = &TEST_DATA[i];
+        test = radio.write(ptr, sizeof(ptr));  
+        Serial.println(test);
+        TEST_ASSERT_EQUAL(test, true);   
+    }
+    for(int i = 0; i <10; i++){
+        ptr = &TEST_DATA[i];
+        TEST_ASSERT_EQUAL_INT(ptr, (RX_ADDR_P0 + i));
+        TEST_ASSERT_EQUAL_INT(ptr, (TX_ADDR + i));
+    }
+}
+
+void setup() {
+    delay(2000);
+    UNITY_BEGIN();
+    RUN_TEST(test_chip_call);
+    RUN_TEST(test_byte_address);
+    RUN_TEST(test_write);
+    radio.setPALevel(RF24_PA_MIN);
+}
+
+void loop() {
+    UNITY_END();
+}
--- test/ARDUINO_UNO_R3/test_uno/test_potentiometer/test_potentiometer.cpp
+++ test/ARDUINO_UNO_R3/test_uno/test_potentiometer/test_potentiometer.cpp
@@ -23,6 +23,7 @@ This process will repeat for as long as the test conditions and program are acti
 3. Transmit converted degree values to servo control.
 4. Run tests for servo control:
   a. Calibration test
+  b. Manual control test
 */
 
 
@@ -32,19 +33,59 @@ This process will repeat for as long as the test conditions and program are acti
 
 //**************** TEST START ****************
 
-
 // HEADER FILE/PREPROCESSOR LIBRARY INITIALIZATION
 #include <Arduino.h>
 #include <Servo.h>
 #include <unity.h>
 
+// UNO PARAMETERS
+const int ANALOG_PIN_COUNT_UNO = 6;
+const int PWM_PIN_COUNT_UNO = 6;
+
+const int ANALOG_PINS_UNO[ANALOG_PIN_COUNT_UNO] = { A0, A1, A2, A3, A4, A5 };
+const int PWM_PINS_UNO[PWM_PIN_COUNT_UNO] = { 3, 5, 6, 9, 10, 11 };
+
+// SERVO AND ASSEMBLY INITIALIZATION
+const int ASSEMBLY_COUNT = 2;
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+Servo servo5;
+Servo servo6;
+
+Servo servoArray[ASSEMBLY_COUNT] = { servo1, servo2 };
+
+// TEST PARAMETERS
+const int MIN_ANGLE = 0;
+const int MID_ANGLE = 90;
+const int MAX_ANGLE = 180;
+
+const int TEST_ANGLE_COUNT = 7;
+const int TEST_ANGLE[TEST_ANGLE_COUNT] = { 0, 30, 60, 90, 120, 150, 180 };
+
+// DECLARATIONS
+int analogInput[ASSEMBLY_COUNT] = {};
+
+const int OUTPUT_BUFFER_SIZE = 100;
+char outputBuffer[OUTPUT_BUFFER_SIZE] = {};
+
+
+
 // FUNCTION PROTOTYPES
+void test_verifySettings();
+void test_startingPosition();
+void test_calibrateRange();
+void test_calibrateInterval();
 void test_calibration();
+void test_manualControl();
 
 // TEST SETUP
 void setUp(void)
 {
-  
+
+
 }
 
   
@@ -54,123 +95,233 @@ void tearDown(void)
 
 }
 
-// PROGRAM SETTINGS
-const int SERVO_COUNT = 1;
-
-const int PWM_CONTROL_PIN[SERVO_COUNT] = { 3 };
-
-const int POTENTIOMETER_COUNT = 1;
-const int ANALOG_CONTROL_PIN[POTENTIOMETER_COUNT] = { A0 };
-
-const int MIN_ANGLE = 0;
-const int MID_ANGLE = 90;
-const int MAX_ANGLE = 180;
-
-const int TEST_ANGLE_COUNT = 7;
-const int TEST_ANGLE[TEST_ANGLE_COUNT] = { 0, 30, 60, 90, 120, 150, 180 };
-
-int testCount = 0;
-int adjustedTestCount = testCount + 1;
-
-const int BUFFER_SIZE = 100;
-char outputBuffer[BUFFER_SIZE] = {};
-
-// DECLARATIONS
-int angle;
-int analogInput;
-
-// SERVO INITIALIZATION
-Servo servo1;
-
-Servo servoArray[SERVO_COUNT] = { servo1 };
 
 // SETUP FUNCTION INITIALIZATION
 void setup() {
 
-  // MONITOR DELAY
-  delay(2000);
+	// MONITOR DELAY
+	delay(2000);
 
 	// MONITOR SPEED
 	Serial.begin(115200);
 
-	// DECLARATION
-  // N/A
-
-  // OUTPUT PIN SETUP
-  for (int index = 0; index < SERVO_COUNT; index++)
-  {
-    pinMode(PWM_CONTROL_PIN[index], OUTPUT);
-  }
-
-  // INPUT
-  // SET INPUT
-  analogInput = analogRead(ANALOG_CONTROL_PIN[0]);
-
-	// SERVO SETUP
-  for (int index = 0; index < SERVO_COUNT; index++)
-  {
-    // ATTACH SERVO
-    servoArray[index].attach(PWM_CONTROL_PIN[index]);
-
-    // STARTING POSITION
-    servoArray[index].write(MID_ANGLE);
-  }
-
-  delay(2000);
-
-  // INITIAL TEST
-  sprintf(outputBuffer, "TEST %d: MOVE TO %d DEGREES", adjustedTestCount ,TEST_ANGLE[testCount]);
-  Serial.println(outputBuffer);
-
-
 	// TEST BEGIN
 	UNITY_BEGIN();
+
+	// OUTPUT PIN SETUP
+	for (int index = 0; index < ASSEMBLY_COUNT; index++)
+		pinMode(PWM_PINS_UNO[index], OUTPUT);
+
+	// INITIAL INPUT AND POSITION SETUP
+	for (int index = 0; index < ASSEMBLY_COUNT; index++)
+	{
+		analogInput[index] = analogRead(ANALOG_PINS_UNO[index]);
+		servoArray[index].attach(PWM_PINS_UNO[index]);
+
+	}
+
+	RUN_TEST(test_verifySettings);
+
+	RUN_TEST(test_calibration);
 
 }
 
 // LOOP FUNCTION INITIALIZATION
 void loop() {
 
+
 	// PROCESS
-  RUN_TEST(test_calibration);
+	RUN_TEST(test_manualControl);
+
+	delay(2000);
+
+	RUN_TEST(test_startingPosition);
 
 	// OUTPUT
-  // N/A
+	// N/A
 
 	// TEST END
-  UNITY_END();
+	UNITY_END();
 
 }
 
 //**************** TEST END ****************
 
 // FUNCTION DEFINITIONS
+void test_verifySettings()
+{
+	int elementCount1;
+	int elementCount2;
+	int elementCount3;
+
+	elementCount1 = sizeof(ANALOG_PINS_UNO) / sizeof(ANALOG_PINS_UNO[0]);
+	elementCount2 = sizeof(PWM_PINS_UNO) / sizeof(PWM_PINS_UNO[0]);
+	elementCount3 = sizeof(servoArray) / sizeof(servoArray[0]);
+
+	TEST_ASSERT_EQUAL_INT(elementCount1, elementCount2);
+	TEST_ASSERT_EQUAL_INT(elementCount2, elementCount1);
+
+	TEST_ASSERT_EQUAL_INT(elementCount3, ASSEMBLY_COUNT);
+}
+
 void test_calibration()
 {
+	RUN_TEST(test_startingPosition);
 
-  while (testCount < TEST_ANGLE_COUNT)
+	delay(2000);
+
+	RUN_TEST(test_calibrateRange);
+
+	delay(2000);
+
+	RUN_TEST(test_calibrateInterval);
+
+	delay(2000);
+
+	RUN_TEST(test_startingPosition);
+}
+
+void test_startingPosition()
+{
+	for (int index = 0; index < ASSEMBLY_COUNT; index++)
+	{
+		servoArray[index].write(MID_ANGLE);
+		TEST_ASSERT_EQUAL_INT(MID_ANGLE, servoArray[index].read());
+	}
+}
+
+void test_calibrateRange()
+{
+	const int ROTATIONS = 3;
+
+	for (int count = 0; count < ROTATIONS; count++)
+	{
+		for (int index = 0; index < ASSEMBLY_COUNT; index++)
+		{
+			servoArray[index].write(MAX_ANGLE);
+			TEST_ASSERT_EQUAL_INT(MAX_ANGLE, servoArray[index].read());
+		}
+
+		
+		delay(1000);
+
+		for (int index = 0; index < ASSEMBLY_COUNT; index++)
+		{
+			servoArray[index].write(MIN_ANGLE);
+			TEST_ASSERT_EQUAL_INT(MIN_ANGLE, servoArray[index].read());
+		}
+
+
+		delay(1000);
+
+		for (int index = 0; index < ASSEMBLY_COUNT; index++)
+		{
+			servoArray[index].write(MID_ANGLE);
+			TEST_ASSERT_EQUAL_INT(MID_ANGLE, servoArray[index].read());
+		}
+	}
+}
+
+void test_calibrateInterval()
+{
+  for (int count = 0; count < (TEST_ANGLE_COUNT); count++)
   {
-    // DEGREE CONVERSION
-    analogInput = analogRead(ANALOG_CONTROL_PIN[0]);
-    angle = map(analogInput, 0, 1023, 0, 180);
-    
-    for (int index = 0; index < SERVO_COUNT; index++)
-      servoArray[index].write(angle);
-
-    if (angle == TEST_ANGLE[testCount] && testCount < TEST_ANGLE_COUNT)
+    for (int index = 0; index < ASSEMBLY_COUNT; index++)
     {
-      TEST_ASSERT_EQUAL_INT_MESSAGE(TEST_ANGLE[testCount], angle, "SUCCESS");
-      Serial.println();
-
-      adjustedTestCount++;
-      testCount++;
-
-      sprintf(outputBuffer, "TEST %d: MOVE TO %d DEGREES", adjustedTestCount ,TEST_ANGLE[testCount]);
-      Serial.println(outputBuffer);
-      
-
+      servoArray[index].write(TEST_ANGLE[count]);
+      TEST_ASSERT_EQUAL_INT(TEST_ANGLE[count], servoArray[index].read());
     }
+
+    delay(1000);
   }
+
+  for (int count = TEST_ANGLE_COUNT - 1; count > (-1); count--)
+  {
+    for (int index = 0; index < ASSEMBLY_COUNT; index++)
+    {
+      servoArray[index].write(TEST_ANGLE[count]);
+      TEST_ASSERT_EQUAL_INT(TEST_ANGLE[count], servoArray[index].read());
+    }
+
+    delay(1000);
+  }
+    
+  for (int index = 0; index < ASSEMBLY_COUNT; index++)
+  {
+    servoArray[index].write(MIN_ANGLE);
+	TEST_ASSERT_EQUAL_INT(MIN_ANGLE, servoArray[index].read());
+  }
+
+}
+
+void test_manualControl()
+{
+	int testCount = 0;
+	int testCountControl = TEST_ANGLE_COUNT - 1;
+	int printCount = testCount + 1;
+
+	int lowerInterval;
+	int upperInterval;
+	const int INTERVAL_SIZE = 10;
+
+	bool servosAligned = 1;
+
+	bool justPrinted[ASSEMBLY_COUNT] = {};
+
+	sprintf(outputBuffer, "TEST %d OF %d: ROTATE TO %d DEGREES", printCount, TEST_ANGLE_COUNT, TEST_ANGLE[testCount]);
+	Serial.println(outputBuffer);
+
+	while(testCount < TEST_ANGLE_COUNT)
+	{
+		for (int index = 0; index < ASSEMBLY_COUNT; index++)
+		{
+			analogInput[index] = map(analogRead(ANALOG_PINS_UNO[index]), 0, 1023, 0, 180);
+			servoArray[index].write(analogInput[index]);
+
+			lowerInterval = TEST_ANGLE[testCount] - INTERVAL_SIZE;
+			upperInterval = TEST_ANGLE[testCount] + INTERVAL_SIZE;
+
+			if ((servoArray[index].read() <= upperInterval) && (servoArray[index].read() >= lowerInterval) && (justPrinted[index] == 0))
+			{
+				sprintf(outputBuffer, "SERVO %d: ALIGNED TO %d WITH DELTA %d, PRINT %d and %d", index, TEST_ANGLE[testCount], INTERVAL_SIZE, justPrinted[0], justPrinted[1]);
+				Serial.println(outputBuffer);
+
+				// TODO: TROUBLESHOOT PRINT FLAGS
+				justPrinted[index] = 1;
+			}
+			else if (((servoArray[index].read() > upperInterval) || (servoArray[index].read() < lowerInterval)) && justPrinted[index] == 1)
+			{
+				justPrinted[index] = 0;
+			}
+
+			servosAligned = 1;
+
+			for (int count = 0; count < ASSEMBLY_COUNT; count++)
+			{
+				if ((servoArray[count].read() > upperInterval) || (servoArray[count].read() < lowerInterval))
+				{
+					servosAligned = 0;
+				}
+			}
+
+			if (servosAligned)
+			{
+				TEST_ASSERT_INT_WITHIN(INTERVAL_SIZE, TEST_ANGLE[testCount], servoArray[index].read());
+
+				sprintf(outputBuffer, "SUCCESS");
+				Serial.println(outputBuffer);
+
+					testCount++;
+					printCount++;
+
+				if (testCount <= testCountControl)
+				{
+					sprintf(outputBuffer, "TEST %d OF %d: ROTATE TO %d DEGREES", printCount, TEST_ANGLE_COUNT, TEST_ANGLE[testCount]);
+					Serial.println(outputBuffer);
+				}
+			}
+		}
+	}
 }
 
 
